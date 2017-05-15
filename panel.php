a236ae3fd05a17c4fa8d469581e9405609196a27
stela24 <stela.24@abv.bg>
2017-05-15 20:02:39 +0300

    стилване

--- panel.php
+++ panel.php
@@ -2,33 +2,40 @@
 session_start();
 ?>
 <head>
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/jquery.fullPage.css">
-        <link rel="stylesheet" href="css/owl.carousel.css">
-        <link rel="stylesheet" href="css/owl.theme.default.css">
-        <link rel="stylesheet" href="css/lightbox.css">
-        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Poiret+One&amp;subset=cyrillic" rel="stylesheet">
+        <link rel="stylesheet" href="css/admin.css">
 </head>
-<form method='post' action='panel.php'>
-Username:
-<input type='text' name='username'>
-Password:
-<input type='password' name='passwd'>
-Submit:
-<input type='submit' value='Submit'>
-<?php
+
+<body>
+        <div class="panel">
+                <div class="panel-container">
+                <div class="title-container">
+                        <div class="title">
+                          <h1>Вход</h1>
+                        </div>
+                </div>
+                <form method='post' action='panel.php'>
+                Username:
+                <input type='text' name='username'>
+                Password:
+                <input type='password' name='passwd'>
+                <input type='submit' value='Submit' class="panel-submit">
+                <?php
 
 
-$username = 'silvia';
-$password = 'q5w1e3';
+                $username = 'silvia';
+                $password = 'q5w1e3';
 
-if(isset($_POST['username'])){
-if($username==$_POST['username']&&$password==$_POST['passwd']){
-	
-	$_SESSION['username']='admin';
-	if($_SESSION['username']=='admin')
-	header('Location: /Photography/admin.php');
+                if(isset($_POST['username'])){
+                if($username==$_POST['username']&&$password==$_POST['passwd']){
+                	
+                	$_SESSION['username']='admin';
+                	if($_SESSION['username']=='admin')
+                	header('Location: /Photography/admin.php');
 
-}
-}
-?>
+                }
+                }
+                ?>
+                </div>
+        </div>
+</body>
